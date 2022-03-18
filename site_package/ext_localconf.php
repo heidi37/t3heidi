4a927d45f1c79b11f1a9562e1afd34c019f6bf69
Heidi Fryzell <hfryzell@gmail.com>
2022-03-18 19:58:54 -0400

    basic backend template and fluid front end configured

---
+++ site_package/ext_localconf.php
@@ -0,0 +1,9 @@
+<?php
+
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
+ExtensionManagementUtility::addPageTSConfig(
+    "@import 'EXT:site_package/Configuration/TsConfig/Page/Page.tsconfig'"
+);
+
+
